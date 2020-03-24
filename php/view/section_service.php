b5f023bf6ce03647833f2f6b82eea347a39130b7
sadadine <sadadfils@gmail.com>
2020-03-24 17:03:24 +0100

    modif

--- php/view/section_service.php
+++ php/view/section_service.php
@@ -1,3 +1,21 @@
 <section  id="section_service" class="service">
 
+<h2>NOS SERVICES</h2>
+        <OL>
+            <li>RENSEIGNEMENT ET INFORMATION PARS PAYS DES DESTINATIONS</li>
+            <div>culture par pays</div>
+            <p> ATA vous renseigne sur les pays où vous souhaitez y aller en vous fournissant toutes les informations vous aimeriez avoir avant votre depart.</p>
+            
+            <li>ACCOMPAGNEMENT INDIVIDUEL SUR VOS VOYAGE</li>
+            <div>SERVICE APRES VENTE</div>
+            <P>ATA organise votre voyage de A à Z , il vous suffit de nous confier votre projet de voyage et nous nous en chargerons du restes  </P>
+
+            <li>RESERVATION DES BILLETS D'AVION ET D'HOTELS </li>
+            <div>RESERVEZ VOS BILLETS ET VOTRE HOTEL</div>
+            <p>Nous vous offrons la possibilite de reserver vos billets d'avions et hotels depuis notre plateforme. nous selectionons,cependant, les meilleurs offres avec des prix defiant toutes concurences</p>
+            
+
+        
+        </OL>
+
    </section>
