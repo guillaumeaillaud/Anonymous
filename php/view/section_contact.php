e3a70c951cdbe3b4ad0024f55734988c11711cdf
exchangezak <exchangezak1@gmail.com>
2020-03-25 00:42:58 +0100

    00h42

--- php/view/section_contact.php
+++ php/view/section_contact.php
@@ -1,27 +1,31 @@
 <section id="section_contact" class="contact">
-    <form id="contact" action="#section_contact.php" method="post">
-        <fieldset>
-            <legend>contacter-nous</legend>
-            <div class="form-group">
-                <label for="nom">Nom <span style="color:red">*</span></label><br>
-                <input type="text" class="form-control" name="nom" id="nom" placeholder="entrer votre nom" required>
-            </div>
-            <div class="form-group">
-                <label for="email">Email <span style="color:red">*</span></label><br>
-                <input type="email" class="form-control" name="email" id="email" placeholder="enter votre email" required>
-            </div>
-            <div class="form-group">
-                <label for="phone">Telephone <span style="color:red">*</span></label><br>
-                <input type="tel" class="form-control" name="telephone" id="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="entrer votre telephone" required><br>
-                <p>Format: 123-456-7890</p>
-            </div>
-            <div class="form-group">
-                <label for="message">message <span style="color:red">*</span></label><br>
-                <textarea name="message" class="form-control" id="message" cols="30" rows="10" placeholder="entrer votre message" required></textarea>
-            </div>
-            <div class="form-group">
-                <input type="submit" value="envoyer">
-            </div>
-        </fieldset>
+    <h2>contactez-nous</h2>
+    <div class="container-fluid">
+        <div class="row">
+            <form class="form" action="#section_contact.php" method="post">
+                <div class="form-group">
+                    <label for="nom">Nom <span style="color:red">*</span></label>
+                    <input type="text" class="form-control" name="nom" id="nom" placeholder="entrer votre nom" required>
+                </div>
+                <div class="form-group">
+                    <label for="email">Email <span style="color:red">*</span></label>
+                    <input type="email" class="form-control" name="email" id="email" placeholder="enter votre email" required>
+                </div>
+                <div class="form-group">
+                    <label for="phone">Telephone <span style="color:red">*</span></label>
+                    <input type="tel" class="form-control" name="telephone" id="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="entrer votre telephone" required>
+                    <small>Format: 123-456-7890</small>
+                </div>
+                <div class="form-group">
+                    <label for="message">Message <span style="color:red">*</span></label>
+                    <textarea name="message" class="form-control" id="message" cols="30" rows="10" placeholder="entrer votre message" required></textarea>
+                </div>
+                <div class="form-group">
+                    <button class="btn btn-outline-dark" type="submit">envoyer</button>
+                </div>
+        </div>
+    </div>
+
+
     </form>
 </section>
