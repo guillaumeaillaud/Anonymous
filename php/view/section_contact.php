9ad014902f65a4ec5c66d0bf9c118afaeb76d269
exchangezak <exchangezak1@gmail.com>
2020-03-24 17:37:39 +0100

    yep

--- php/view/section_contact.php
+++ php/view/section_contact.php
@@ -4,20 +4,20 @@
             <legend>contacter-nous</legend>
             <label for="nom"><span style="color:red">*</span>Nom</label>
             <div>
-            <input type="text" name="nom" id="nom" placeholder="entrer votre nom" required>
+                <input type="text" name="nom" id="nom" placeholder="entrer votre nom" required>
             </div>
             <label for="email"><span style="color:red">*</span>Email</label>
             <div>
-            <input type="email" name="email" id="email" placeholder="enter votre email" required>
+                <input type="email" name="email" id="email" placeholder="enter votre email" required>
             </div>
             <label for="phone"><span style="color:red">*</span>Telephone</label>
             <div>
-            <input type="tel" name="telephone" id="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="entrer votre telephone" required><br>
-            <p>Format: 123-456-7890</p>
+                <input type="tel" name="telephone" id="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="entrer votre telephone" required><br>
+                <p>Format: 123-456-7890</p>
             </div>
             <label for="message"><span style="color:red">*</span>message</label>
             <div>
-            <textarea name="message" id="message" cols="30" rows="10" placeholder="entrer votre message" required></textarea>
+                <textarea name="message" id="message" cols="30" rows="10" placeholder="entrer votre message" required></textarea>
             </div>
             <div>
                 <input type="submit" value="envoyer">
