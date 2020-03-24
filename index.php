02e5c54f862bf9030ac8c8fd15c00839e38b0e6d
angekouakou <kouakou.ange20@yahoo.fr>
2020-03-24 16:14:38 +0100

    index actualisé

--- index.php
+++ index.php
@@ -2,5 +2,8 @@
 <?php
 require_once "php/view/header.php";
 require_once "php/view/section_index.php";
+require_once "php/view/section_service.php";
+require_once "php/view/section_contact.php";
 require_once "php/view/footer.php"; 
 ?>
+
