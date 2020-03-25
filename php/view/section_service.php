a11b84897939f2e1f5962ed3178e2664047caece
sadadine <sadadfils@gmail.com>
2020-03-25 15:25:35 +0100

    ajout img

--- php/view/section_service.php
+++ php/view/section_service.php
@@ -2,7 +2,10 @@
    
 
             <h3>NOS SERVICES</h3>
-            
+
+            <div class="img"><img src="image1.jpeg" alt="photo"></div>
+
+
         <OL>
             <li>RENSEIGNEMENT ET INFORMATION PAR PAYS DES DESTINATIONS</li>
             <div>culture par pays</div>
@@ -16,8 +19,6 @@
             <div>RESERVEZ VOS BILLETS ET VOTRE HOTEL</div>
             <p>Nous vous offrons la possibilite de reserver vos billets d'avions et hotels depuis notre plateforme. nous selectionons,cependant, les meilleurs offres avec des prix defiant toutes concurences</p>
             
-
-        
             <h4> NOS CHOIX </h4>
         </OL>
 
