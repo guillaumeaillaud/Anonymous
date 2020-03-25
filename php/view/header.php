4962ebe0e053e5e66e490072f5dea29be9d93ff9
angekouakou <kouakou.ange20@yahoo.fr>
2020-03-25 14:57:33 +0100

    ancrage

--- php/view/header.php
+++ php/view/header.php
@@ -16,13 +16,13 @@
 <div class="collapse navbar-collapse" id="navbarColor02">
     <ul class="navbar-nav mr-auto">
         <li class="nav-item active">
-            <a class="nav-link" href="#section_index.php">Accueil <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="#section_index">Accueil <span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#section_service.php">Service</a>
+            <a class="nav-link" href="#section_service">Service</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#section_contact.php">Contact</a>
+            <a class="nav-link" href="#section_contact">Contact</a>
         </li>
     </ul>
 
