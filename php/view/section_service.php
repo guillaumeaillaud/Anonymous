01d52fa01e69f25a38c8a91e07206c2764608e3e
guillaumeaillaud <guillaume.aillaud.13@gmail.com>
2020-03-25 19:23:07 +0100

    modif css

--- php/view/section_service.php
+++ php/view/section_service.php
@@ -1,39 +1,41 @@
-<section  id="section_service" class="service">
-   
+<section id="section_service" class="service">
 
-            <h3>NOS SERVICES</h3>
 
-            <div class="img"><img src="image3.jpg" alt="photo"></div>
-    
+    <h3>NOS SERVICES</h3>
+    <div>
+        <img src="image3.jpg" alt="photo">
+        <img src="image7.jpg" alt="photo">
+    </div>
 
+    <div class="service">
         <OL>
             <li>RENSEIGNEMENT ET INFORMATION PAR PAYS DES DESTINATIONS</li>
             <div>culture par pays</div>
             <p> ATA vous renseigne sur les pays où vous souhaitez y aller en vous fournissant toutes les informations vous aimeriez avoir avant votre depart.</p>
-            
+
             <li>ACCOMPAGNEMENT INDIVIDUEL SUR VOS VOYAGE</li>
             <div>SERVICE APRES VENTE</div>
-            <P>ATA organise votre voyage de A à Z , il vous suffit de nous confier votre projet de voyage et nous nous en chargerons du restes  </P>
+            <P>ATA organise votre voyage de A à Z , il vous suffit de nous confier votre projet de voyage et nous nous en chargerons du restes </P>
 
             <li>RESERVATION DES BILLETS D'AVION ET D'HOTELS </li>
             <div>RESERVEZ VOS BILLETS ET VOTRE HOTEL</div>
             <p>Nous vous offrons la possibilite de reserver vos billets d'avions et hotels depuis notre plateforme. nous selectionons,cependant, les meilleurs offres avec des prix defiant toutes concurences</p>
-            
+
             <h4> NOS CHOIX </h4>
 
-            <div class="img"><img src="image7.jpg" alt="photo"></div>
+
 
         </OL>
 
 
         <ul>
-        <li> accompagnement passionné, dynamique & enthousiaste.</li>
-        <li>Des petits groupes de 8 à 25 personnes.</li>
-        <li>Des rencontres avec les populations locales.</li>
-        <li>Des guides expérimentés.</li>
-        <li>Une découverte de l’histoire humaine et spirituelle des lieux traversés.</li>
-        <li>Des rencontre et de nouvelles connaissance.</li>
-    </ul>
+            <li> accompagnement passionné, dynamique & enthousiaste.</li>
+            <li>Des petits groupes de 8 à 25 personnes.</li>
+            <li>Des rencontres avec les populations locales.</li>
+            <li>Des guides expérimentés.</li>
+            <li>Une découverte de l’histoire humaine et spirituelle des lieux traversés.</li>
+            <li>Des rencontre et de nouvelles connaissance.</li>
+        </ul>
 
-
-   </section>
+    </div>
+</section>
