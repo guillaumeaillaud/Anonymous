25a2604380bc9abbb47f79e2de0763af94b690b3
angekouakou <kouakou.ange20@yahoo.fr>
2020-03-30 16:43:10 +0200

    ange k

--- php/view/section_service.php
+++ php/view/section_service.php
@@ -1,43 +1,39 @@
-<section  id="section_service" class="service">
-   
 
-            <h3>NOS SERVICES
-</h3>
+<section id="section_service">
 
-            <div class="img"><img src="image3.jpg" alt="photo"></div>
-    
 
-        <OL>
-            <li>RENSEIGNEMENT ET INFORMATION PAR PAYS DES DESTINATIONS</li>
-            <div>culture par pays</div>
-            <p> ATA vous renseigne sur les pays où vous souhaitez y aller en vous
-                 fournissant toutes les informations vous aimeriez avoir avant votre depart.</p>
-            
-            <li>ACCOMPAGNEMENT INDIVIDUEL SUR VOS VOYAGE</li>
-            <div>SERVICE APRES VENTE</div>
-            <P>ATA organise votre voyage de A à Z , 
-                il vous suffit de nous confier votre projet de voyage et nous nous en chargerons du restes  </P>
+    <h2>NOS OFFRES ET SERVICES</h2><br>
+    <strong><div class="sousT">RENSEIGNEMENT ET INFORMATION PAR PAYS DE DESTINATION</div><br></strong>
+    <div class="paragraphe">
 
-            <li>RESERVATION DES BILLETS D'AVION ET D'HOTELS</li>
-            <div>RESERVEZ VOS BILLETS ET VOTRE HOTEL</div>
-            <p>Nous vous offrons la possibilite de reserver vos billets d'avions
-                 et hotels depuis notre plateforme. nous selectionons,cependant, les meilleurs offres avec des prix defiant toutes concurences</p>
-            
-            <h4> NOS CHOIX </h4>
-
-            <div class="img"><img src="image7.jpg" alt="photo"></div>
-
-        </OL>
-
+    <h3>NOS SERVICES</h3>
 
         <ul>
-        <li> accompagnement passionné, dynamique & enthousiaste.</li>
-        <li>Des petits groupes de 8 à 25 personnes.</li>
-        <li>Des rencontres avec les populations locales.</li>
-        <li>Des guides expérimentés.</li>
-        <li>Une découverte de l’histoire humaine et spirituelle des lieux traversés.</li>
-        <li>Des rencontre et de nouvelles connaissance.</li>
-    </ul>
+            <li>RESERVATION DES BILLETS D'AVION ET D'HOTELS </li>
+            <li>RENSEIGNEMENT SUR LES COMPAGNIES AERIENNES</li>
+            <li>RAPPORT SUR L'ETAT DE SECURITE DU PAYS D'ACCUIEL</li>
+            <li>ACCOMPAGNEMENT ADMINISTRATIF-SERVICE VISA </li>
+            <li>SERVICE AIDE À LA PERSONNE </li>
 
 
-   </section>
+            <p> Nous sommes aussi disponible à repondre à toutes vos preoccupation.</p>  
+            <p> Avec nous,votre satisfaction est assurée.</p>
+</ul>
+            
+            <h4> NOS OFFRES </h4>
+        
+        <ul>
+            <li> accompagnement passionné, dynamique & enthousiaste.</li>
+            <li>Des petits groupes de 8 à 25 personnes.</li>
+            <li>Des rencontres avec les populations locales.</li>
+            <li>Des guides expérimentés.</li>
+            <li>Une découverte de l’histoire humaine et spirituelle des lieux traversés.</li>
+            <li>Des rencontre et de nouvelles connaissance.</li>
+        </ul>
+    </div>
+</section>
+<section class="image" >
+
+       <div class="image1"><img class="image2" src="assets/img/image3.jpg" alt="photo"></div> 
+        <div class="image1"><img class="image2" src="assets/img/image5.jpg" alt="photo"></div>
+</section>
