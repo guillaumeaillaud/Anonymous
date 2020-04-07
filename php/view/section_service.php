6799d74320bf7ee658da62742998d681b0fcffe0
guillaumeaillaud <guillaume.aillaud.13@gmail.com>
2020-04-07 16:59:16 +0200

    modifs css

--- php/view/section_service.php
+++ php/view/section_service.php
@@ -7,7 +7,9 @@
     </strong>
     <div class="paragraphe">
         <div class="paragraphe1">
-            <h3>NOS SERVICES</h3>
+            <div>
+                <h3>NOS SERVICES</h3>
+            </div>
 
             <ul>
                 <li>Réservation de billets d'avion et d'hotel </li>
@@ -16,11 +18,13 @@
                 <li>Accompagnement admninistratif-Services-Visa </li>
                 <li>Service d'aide à la personne</li>
                 <li>Nous sommes aussi disponible à repondre à toutes vos preoccupation.</li>
-                
+
             </ul>
         </div>
         <div class="paragraphe2">
-            <h3> NOS OFFRES </h3>
+            <div>
+                <h3> NOS OFFRES </h3>
+            </div>
 
             <ul>
                 <li>Accompagnement passionné, dynamique & enthousiaste.</li>
@@ -33,9 +37,14 @@
         </div>
     </div>
 </section>
-<strong><div class="sousT"> AVEC NOUS VOTRE SATISFATCTION EST ASSUREE</div></strong>
+<strong>
+    <div class="sousT"> AVEC NOUS VOTRE SATISFATCTION EST ASSUREE</div>
+</strong>
 <section class="image">
 
     <div class="image1"><img class="image2" src="assets/img/image3.jpg" alt="photo"></div>
     <div class="image1"><img class="image2" src="assets/img/image5.jpg" alt="photo"></div>
+    <div class="image1"><img class="image2" src="assets/img/image2.jpg" alt="photo"></div>
+    <div class="image1"><img class="image2" src="assets/img/image1.jpg" alt="photo"></div>
+    
 </section>
