a2cf9c2b203fbb1a4178f0ed97ebb55e094c05ac
guillaumeaillaud <guillaume.aillaud.13@gmail.com>
2020-04-07 16:10:26 +0200

    modifs css

--- php/view/section_service.php
+++ php/view/section_service.php
@@ -1,39 +1,41 @@
-
 <section id="section_service">
 
 
     <h2>NOS OFFRES ET SERVICES</h2><br>
-    <strong><div class="sousT">RENSEIGNEMENT ET INFORMATION PAR PAYS DE DESTINATION</div><br></strong>
+    <strong>
+        <div class="sousT">RENSEIGNEMENT ET INFORMATION PAR PAYS DE DESTINATION</div><br>
+    </strong>
     <div class="paragraphe">
+        <div class="paragraphe1">
+            <h3>NOS SERVICES</h3>
 
-    <h3>NOS SERVICES</h3>
+            <ul>
+                <li>Réservation de billets d'avion et d'hotel </li>
+                <li>Renseignement sur les compagnies aériennes</li>
+                <li>Rapport sur l'état de sécurité du pays d'acceuil</li>
+                <li>Accompagnement admninistratif-Services-Visa </li>
+                <li>Service d'aide à la personne</li>
+                <li>Nous sommes aussi disponible à repondre à toutes vos preoccupation.</li>
+                
+            </ul>
+        </div>
+        <div class="paragraphe2">
+            <h3> NOS OFFRES </h3>
 
-        <ul>
-            <li>RESERVATION DES BILLETS D'AVION ET D'HOTELS </li>
-            <li>RENSEIGNEMENT SUR LES COMPAGNIES AERIENNES</li>
-            <li>RAPPORT SUR L'ETAT DE SECURITE DU PAYS D'ACCUIEL</li>
-            <li>ACCOMPAGNEMENT ADMINISTRATIF-SERVICE VISA </li>
-            <li>SERVICE AIDE À LA PERSONNE </li>
-
-
-            <p> Nous sommes aussi disponible à repondre à toutes vos preoccupation.</p>  
-            <p> Avec nous,votre satisfaction est assurée.</p>
-</ul>
-            
-            <h4> NOS OFFRES </h4>
-        
-        <ul>
-            <li> accompagnement passionné, dynamique & enthousiaste.</li>
-            <li>Des petits groupes de 8 à 25 personnes.</li>
-            <li>Des rencontres avec les populations locales.</li>
-            <li>Des guides expérimentés.</li>
-            <li>Une découverte de l’histoire humaine et spirituelle des lieux traversés.</li>
-            <li>Des rencontre et de nouvelles connaissance.</li>
-        </ul>
+            <ul>
+                <li>Accompagnement passionné, dynamique & enthousiaste.</li>
+                <li>Des petits groupes de 8 à 25 personnes.</li>
+                <li>Des rencontres avec les populations locales.</li>
+                <li>Des guides expérimentés.</li>
+                <li>Une découverte de l’histoire humaine et spirituelle des lieux traversés.</li>
+                <li>Des rencontre et de nouvelles connaissance.</li>
+            </ul>
+        </div>
     </div>
 </section>
-<section class="image" >
+<strong><div class="sousT"> AVEC NOUS VOTRE SATISFATCTION EST ASSUREE</div></strong>
+<section class="image">
 
-       <div class="image1"><img class="image2" src="assets/img/image3.jpg" alt="photo"></div> 
-        <div class="image1"><img class="image2" src="assets/img/image5.jpg" alt="photo"></div>
+    <div class="image1"><img class="image2" src="assets/img/image3.jpg" alt="photo"></div>
+    <div class="image1"><img class="image2" src="assets/img/image5.jpg" alt="photo"></div>
 </section>
