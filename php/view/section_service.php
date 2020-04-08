99c6decb4e4c1603e211425b7682acfe249e2321
exchangezak <exchangezak1@gmail.com>
2020-04-08 21:49:34 +0200

    modif 2 photos

--- php/view/section_service.php
+++ php/view/section_service.php
@@ -5,34 +5,24 @@
     <strong>
         <div class="sousT">RENSEIGNEMENT ET INFORMATION PAR PAYS DE DESTINATION</div><br>
     </strong>
-    <div class="paragraphe">
-        <div class="paragraphe1">
-            <div>
-                <h3>NOS SERVICES</h3>
-                <div class="image1"><img class="image2" src="image10.jpg" alt="photo"></div>
-                
-            </div>
-        <div>
-
-    
-            <ul>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-6">            
+                <h3 class="h3">NOS SERVICES</h3>
+                <img class="rounded float-left" src="image10.jpg" alt="photo">
+            <ul class="liste li1">
                 <li>Réservation de billets d'avion et d'hotel </li>
                 <li>Renseignement sur les compagnies aériennes</li>
                 <li>Rapport sur l'état de sécurité du pays d'acceuil</li>
                 <li>Accompagnement admninistratif-Services-Visa </li>
                 <li>Service d'aide à la personne</li>
                 <li>Nous sommes aussi disponible à repondre à toutes vos preoccupation.</li>
-
             </ul>
-        </div>
-        <div class="paragraphe2">
-            <div>
-                <h3> NOS OFFRES </h3>
-                <div class="image1"><img class="image2" src="image11.jpg" alt="photo"></div>
-                
-            </div>
-
-            <ul>
+         </div>
+            <div class="col-md-6"> 
+                <h3 class="h3"> NOS OFFRES </h3>
+                <img class="rounded float-rigth" src="image11.jpg" alt="photo">
+            <ul class="liste li2">
                 <li>Accompagnement passionné, dynamique & enthousiaste.</li>
                 <li>Des petits groupes de 8 à 25 personnes.</li>
                 <li>Des rencontres avec les populations locales.</li>
@@ -40,6 +30,8 @@
                 <li>Une découverte de l’histoire humaine et spirituelle des lieux traversés.</li>
                 <li>Des rencontre et de nouvelles connaissance.</li>
             </ul>
+            
+        </div>
         </div>
     </div>
 </section>
