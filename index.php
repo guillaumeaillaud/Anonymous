3020e4791dbf7ea04e0093b6e0fa6a3ebc22d07b
exchangezak <exchangezak1@gmail.com>
2020-05-15 23:45:05 +0200

    zak

--- index.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-require_once "php/view/header.php";
-require_once "php/view/section_index.php";
-require_once "php/view/section_service.php";
-require_once "php/view/section_contact.php";
-require_once "php/view/footer.php"; 
-?>
-
